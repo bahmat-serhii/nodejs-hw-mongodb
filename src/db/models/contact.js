d7b00a1d8c516888d73183b31eb08abc3f1bc769
Bakhmatov_Serhii <bakhmatov1982@gmail.com>
2025-07-23 20:09:15 +0300

    add files

---
+++ src/db/models/contact.js
@@ -0,0 +1,37 @@
+// src/db/models/student.js
+
+import { model, Schema } from 'mongoose';
+
+const contactsSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    contactType: {
+      type: String,
+      required: true,
+      enum: ['work', 'home', 'personal'],
+      default: 'personal',
+    },
+    email: {
+      type: String,
+      required: false,
+    },
+    isFavourite: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  },
+);
+
+export const ContactsCollection = model('contacts', contactsSchema);
