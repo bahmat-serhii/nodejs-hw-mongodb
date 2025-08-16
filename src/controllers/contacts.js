b6a54153e99ebb1ad6b6820a170fab4ad436c4ad
Bakhmatov_Serhii <bakhmatov1982@gmail.com>
2025-08-16 10:24:43 +0300

    fix bags

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -53,8 +53,22 @@ export const getContactByIdController = async (req, res) => {
 };
 
 export const createContactController = async (req, res) => {
+  const photo = req.file;
   const userId = req.user._id;
-  const contact = await createContact({ ...req.body, userId });
+  let photoUrl;
+  if (photo) {
+    if (getEnvVar('ENABLE_CLOUDINARY') === 'true') {
+      photoUrl = await saveFileToCloudinary(photo);
+    } else {
+      photoUrl = await saveFileToUploadDir(photo);
+    }
+  }
+
+  const contact = await createContact({
+    ...req.body,
+    userId,
+    photo: photoUrl,
+  });
 
   res.status(201).json({
     status: 201,
